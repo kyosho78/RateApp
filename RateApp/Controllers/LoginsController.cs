5938ffb331fd6e6ad95166323b5e20acb4ed04be
Valter Backström <dylandante@gmail.com>
2024-08-09 14:09:50 +0300

    Controllers added

---
+++ RateApp/Controllers/LoginsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RateApp.Models;
+
+namespace RateApp.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private RatingDBEntities db = new RatingDBEntities();
+
+        // GET: Logins
+        public ActionResult Index()
+        {
+            var logins = db.Logins.Include(l => l.Users).Include(l => l.Suppliers).Include(l => l.Users1);
+            return View(logins.ToList());
+        }
+
+        // GET: Logins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logins);
+        }
+
+        // GET: Logins/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LoginId,UserId,LoginTime,LogoutTime,IpAddress,SupplierId")] Logins logins)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(logins);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", logins.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            return View(logins);
+        }
+
+        // GET: Logins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", logins.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            return View(logins);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LoginId,UserId,LoginTime,LogoutTime,IpAddress,SupplierId")] Logins logins)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(logins).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", logins.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", logins.UserId);
+            return View(logins);
+        }
+
+        // GET: Logins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logins logins = db.Logins.Find(id);
+            if (logins == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logins);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Logins logins = db.Logins.Find(id);
+            db.Logins.Remove(logins);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RateApp/Controllers/RatingsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RateApp.Models;
+
+namespace RateApp.Controllers
+{
+    public class RatingsController : Controller
+    {
+        private RatingDBEntities db = new RatingDBEntities();
+
+        // GET: Ratings
+        public ActionResult Index()
+        {
+            var ratings = db.Ratings.Include(r => r.Suppliers).Include(r => r.Users);
+            return View(ratings.ToList());
+        }
+
+        // GET: Ratings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ratings ratings = db.Ratings.Find(id);
+            if (ratings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ratings);
+        }
+
+        // GET: Ratings/Create
+        public ActionResult Create()
+        {
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        }
+
+        // POST: Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RatingId,UserId,SupplierId,RatingValue,Comment,CreatedAt,UpdatedAt")] Ratings ratings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ratings.Add(ratings);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", ratings.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", ratings.UserId);
+            return View(ratings);
+        }
+
+        // GET: Ratings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ratings ratings = db.Ratings.Find(id);
+            if (ratings == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", ratings.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", ratings.UserId);
+            return View(ratings);
+        }
+
+        // POST: Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RatingId,UserId,SupplierId,RatingValue,Comment,CreatedAt,UpdatedAt")] Ratings ratings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ratings).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SupplierId = new SelectList(db.Suppliers, "SupplierId", "SupplierName", ratings.SupplierId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", ratings.UserId);
+            return View(ratings);
+        }
+
+        // GET: Ratings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ratings ratings = db.Ratings.Find(id);
+            if (ratings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ratings);
+        }
+
+        // POST: Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ratings ratings = db.Ratings.Find(id);
+            db.Ratings.Remove(ratings);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
